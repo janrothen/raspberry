875d1582384abd67ed2afde7afae01b7ecb58b81
Jan Rothen <jan.rothen@me.com>
2019-03-18 21:34:35 +0100

    Refactoring
    - Simplified program flow
    - Filtering & sorting payments

--- cryptocurrency/check_stellar_inflation_payments.py
+++ cryptocurrency/check_stellar_inflation_payments.py
@@ -29,41 +29,31 @@ class Payment(object):
 		self._amount = 0
 		self._from = None
 
-
-def check_is_receiving_payments():
-	payment = None
-	email_msg = ''
-
-	try:
-		payment = get_last_inflation_payment()
-	except Exception as e:
-		email_msg += str(e)
+def check_payment():
+	payment = get_last_inflation_payment()
 
 	if not is_sufficient(payment):
-		email_msg += assemble_msg(payment)
+		email_msg = assemble_msg(payment)
 		email = Email()
 		email.send(ALERT_MSG_SUBJECT, email_msg, ADDR_TO)
 
 def get_last_inflation_payment():
-	request = Request()
-	data = fetch_last_payments(request)
-	payments = map_payments(data)
-	return last_inflation_payment(payments)
+	try:
+		data = fetch_last_payments_data()
+		payments = map_payments(data)
+		return last_inflation_payment(payments)
+	except Exception as e:
+		return None
 
-def fetch_last_payments(request):
+def fetch_last_payments_data():
 	data = {}
-	url = url_last_payments(ADDRESS)
+	url = url_last_payments()
+
+	request = Request()
 	result = request.get(url)
 	if result:
 		data = json.loads(result)
 	return data
-
-def last_inflation_payment(payments):
-	for payment in payments:
-		if (payment.addr_from == INFLATION_DESTINATION):
-			return payment
-
-	return None
 
 def map_payments(data):
 	transactions = data['_embedded']['records']
@@ -83,14 +73,38 @@ def map_payment(data):
 	p.addr_from = data['from']
 	return p
 
+def is_sufficient(payment):
+	if (not payment):
+		return False
+
+	return payment.amount > THRESHOLD_AMOUNT and days_since_last_payment(payment.date) < THRESHOLD_DAYS
+
+def last_inflation_payment(payments):
+	filtered = list(filter(lambda p: p.addr_from == INFLATION_DESTINATION, payments))
+	if (filtered):
+		filtered.sort(key=lambda p: p.date, reverse=False)
+		return filtered[0]
+	return None
+
+def is_inflation_payment(payment):
+	return 
+
+def get_account_balance():
+	try:
+		account = get_account()
+		return account['balance']
+	except Exception as e:
+		return 0
+
 def get_account():
-	request = Request()
-	data = fetch_account(request)
+	data = fetch_account_data()
 	return map_account(data)
 
-def fetch_account(request):
+def fetch_account_data():
 	data = {}
-	url = url_account(ADDRESS)
+	url = url_account()
+
+	request = Request()
 	result = request.get(url)
 	if result:
 		data = json.loads(result)
@@ -101,12 +115,6 @@ def map_account(data):
 	mapped['balance'] = data['balances'][0]['balance']
 	return mapped
 
-def is_sufficient(payment):
-	if (not payment):
-		return False
-
-	return payment.amount > THRESHOLD_AMOUNT and days_since_last_payment(payment.date) < THRESHOLD_DAYS
-	
 def days_since_last_payment(date):
 	now = datetime.now(timezone.utc)
 	return abs((now - date).days)
@@ -126,30 +134,25 @@ def assemble_msg(payment):
 	return ALERT_MSG_NONE.format(
 			balance=balance)
 
-def get_account_balance():
-	try:
-		account = get_account()
-		return account['balance']
-	except Exception as e:
-		return 0
-
-def url_last_payments(address):
-	url = '{endpoint}/accounts/{address}/payments?limit=10&order=desc'
+# URLs
+def url_last_payments():
+	url = '{endpoint}/accounts/{address}/payments?limit={limit}&order=desc'
 	url = url.format(
 		endpoint=SERVICE_ENDPOINT,
-		address=address)
+		address=ADDRESS,
+		limit=10) # increase if necessary
 	return url
 
-def url_account(address):
+def url_account():
 	url = '{endpoint}/accounts/{address}'
 	url = url.format(
 		endpoint=SERVICE_ENDPOINT,
-		address=address)
+		address=ADDRESS)
 	return url
 
 def run():
 	try:
-		check_is_receiving_payments()
+		check_payment()
 	except:
 		traceback.print_exc(file=sys.stdout)
 		sys.exit(0)
