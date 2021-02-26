7e5b4889791884329bce117b2d19a7fc7049a4f8
Jan Rothen <jan.rothen@me.com>
2021-02-26 11:45:07 +0100

    wip

--- epaper/run.py
+++ epaper/run.py
@@ -33,33 +33,18 @@ try:
     frame = Image.new('1', (epd.height, epd.width), 0)  # 255: clear the frame
     bitcoin = Image.open(os.path.join(DIR_MEDIA, 'bitcoin122x122_b.bmp'))
     frame.paste(bitcoin, (64,0))    
-    #epd.display(epd.getbuffer(frame))
+    epd.display(epd.getbuffer(frame))
     time.sleep(5)
 
     epd.Clear(0xFF)
 
-    # Drawing on the image
-    font15 = ImageFont.truetype(os.path.join(DIR_MEDIA, FONT), 15)
     font36 = ImageFont.truetype(os.path.join(DIR_MEDIA, FONT), 36)
     
-    logging.info("1.Drawing on the image...")
-    image = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame    
-    draw = ImageDraw.Draw(image)
-    
-    draw.rectangle([(0,0),(50,50)],outline = 0)
-    draw.rectangle([(55,0),(100,50)],fill = 0)
-    draw.line([(0,0),(50,50)], fill = 0,width = 1)
-    draw.line([(0,50),(50,0)], fill = 0,width = 1)
-    draw.chord((10, 60, 50, 100), 0, 360, fill = 0)
-    draw.ellipse((55, 60, 95, 100), outline = 0)
-    draw.pieslice((55, 60, 95, 100), 90, 180, outline = 0)
-    draw.pieslice((55, 60, 95, 100), 270, 360, fill = 0)
-    draw.polygon([(110,0),(110,50),(150,25)],outline = 0)
-    draw.polygon([(190,0),(190,50),(150,25)],fill = 0)
-    draw.text((120, 60), 'e-Paper demo', font = font15, fill = 0)
-    draw.text((110, 90), u'$48''231', font = font36, fill = 0)
-    epd.display(epd.getbuffer(image))
-    time.sleep(2)
+    frame = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame    
+    price = ImageDraw.Draw(frame)
+    price.text((32, 32), u'$48''231', font = font36, fill = 0)
+    epd.display(epd.getbuffer(frame))
+    time.sleep(5)
     
     # read bmp file 
     logging.info("2.read bmp file...")
